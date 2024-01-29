67281cdccb15a41e2f4fb81ade1970dff0598b1f
Alexander Buck <abuck@spear.ai>
2024-01-29 18:04:17 -0500

    Only 10 runs, 99 is _a lot_

--- docker_minio_error_reproduction/__main__.py
+++ docker_minio_error_reproduction/__main__.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     boto3_write(1_900_000_000)
 
     # Reading large file
-    for i in range(99):
+    for i in range(10):
         with multiprocessing.Pool(processes=N) as pool:
             pool.map(boto3_read, range(N))
